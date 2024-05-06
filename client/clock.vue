4be23f40a1fab780e3a02273d793a7bb19522f73
windbullet <2536810643@qq.com>
2024-05-06 10:16:46 +0800

    更新

--- client/clock.vue
+++ client/clock.vue
@@ -5,9 +5,21 @@
 </template>
 
 <script lang="ts" setup>
+  import { useConfig } from '@koishijs/client';
   import { ref } from 'vue'
-  let time = ref(new Date().toLocaleString())
+  const config = useConfig()
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  } as const;
+
+  let time = ref(new Date().toLocaleString(config.value.locale, options))
   setInterval(() => {
-    time.value = new Date().toLocaleString()
+    time.value = new Date().toLocaleString(config.value.locale, options)
   }, 5)
 </script>
